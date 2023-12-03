56fbe0023468c7eb0cf40064388e8e4d914e0cb9
Андрей Мещеринов <androidmesch@mail.ru>
2023-12-03 19:47:01 +0300

    Style - page-header, wrapper , global.scss.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('andrey');
+const mainNav = document.querySelector('.page-header__nav');
+const mainNavToggle =  document.querySelector('.page-header__button-nav');
+
+if(mainNav) {
+  mainNavToggle.addEventListener('click', () => {
+    mainNav.classList.remove('page-header__nav--opened');
+    mainNav.classList.toggle('page-header__nav--closed');
+  })
+}
