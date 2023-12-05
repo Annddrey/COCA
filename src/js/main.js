44ff7bcfc6eb7b541ceaf7baa94e7cdbf81bf08b
Андрей Мещеринов <androidmesch@mail.ru>
2023-12-05 18:59:00 +0300

    Style - page-header.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
+const pageHeader = document.querySelector('.page-header');
 const mainNav = document.querySelector('.page-header__nav');
 const mainNavToggle =  document.querySelector('.page-header__button-nav');
 
-if(mainNav) {
+if(mainNavToggle) {
   mainNavToggle.addEventListener('click', () => {
-    mainNav.classList.remove('page-header__nav--opened');
+    mainNav.classList.toggle('page-header__nav--opened');
     mainNav.classList.toggle('page-header__nav--closed');
+
+    pageHeader.classList.toggle('page-header--opened-menu');
   })
 }
