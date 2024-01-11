89934ac8f2dc0b454cb34c6702c9fc7d72163067
Андрей Мещеринов <androidmesch@mail.ru>
2024-01-11 19:59:07 +0300

    Style - news.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
-const pageHeader = document.querySelector('.page-header');
-const mainNav = document.querySelector('.page-header__nav');
-const mainNavToggle =  document.querySelector('.page-header__button-nav');
+const pageHeader = document.querySelector(".page-header");
+const mainNav = document.querySelector(".page-header__nav");
+const mainNavToggle = document.querySelector(".page-header__button-nav");
 
-if(mainNavToggle) {
-  mainNavToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('page-header__nav--opened');
-    mainNav.classList.toggle('page-header__nav--closed');
+mainNavToggle &&
+  mainNavToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("page-header__nav--opened");
+    mainNav.classList.toggle("page-header__nav--closed");
 
-    pageHeader.classList.toggle('page-header--opened-menu');
-  })
-}
+    pageHeader.classList.toggle("page-header--opened-menu");
+  });
